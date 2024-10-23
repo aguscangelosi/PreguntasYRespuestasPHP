7ed4c82d42b1a5c8b8c5a74ca36935e3d9363185
MartinMeza22 <martinmeza.t22@gmail.com>
2024-10-22 22:22:14 -0300

    Email -> Terminado

--- helper/FileEmailSender.php
+++ helper/FileEmailSender.php
@@ -1,36 +1,37 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 include_once './vendor/PHPMailer/src/PHPMailer.php';
 include_once './vendor/PHPMailer/src/SMTP.php';
 include_once './vendor/PHPMailer/src/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class MailService {
     private $mail;
 
     public function __construct() {
-        $this->mail = new PHPMailer(true);  // Habilitar excepciones
-        $this->mail->isSMTP();              // Usar SMTP
-        $this->mail->Host = 'smtp.gmail.com';  // Cambiar según tu proveedor SMTP
+        $this->mail = new PHPMailer(true);
+        $this->mail->isSMTP();
+        $this->mail->Host = 'smtp.gmail.com';
         $this->mail->SMTPAuth = true;
-        $this->mail->Username = 'psolomeo2000@gmail.com'; // Tu correo
-        $this->mail->Password = 'PSolomeo2000__';     // Tu contraseña
+
+        // Estos datos deben almacenarse de forma segura
+        $this->mail->Username = 'facundortega1234@gmail.com';
+        $this->mail->Password = 'tyjc nybr ypbn bbpw';
+
         $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $this->mail->Port = 587;
+
     }
 
     public function sendMail($to, $subject, $body) {
         try {
-            // Configuración del correo
-            $this->mail->setFrom('psolomeo2000@gmail.com', 'Solomeo');
-            $this->mail->addAddress('facundortega1234@gmail.com');
+            $this->mail->setFrom('facundortega1234@gmail.com', 'Solomeo');
+            $this->mail->addAddress($to);   // Usa la dirección que se pasa como parámetro
             $this->mail->isHTML(true);
-            $this->mail->Subject = "Validar password";
-            $this->mail->Body    = "Texto de prueba";
-
-            // Enviar correo
+            $this->mail->Subject = $subject;
+            $this->mail->Body = $body;
             $this->mail->send();
             return true;
         } catch (Exception $e) {
