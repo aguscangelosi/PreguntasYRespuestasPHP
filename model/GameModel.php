e05bc46a4fdb64087a01ee06fa52e969dc394a9c
Facu Ortega <fortega@finnegans.com.ar>
2024-11-04 13:55:03 -0300

    se suma game model

--- model/GameModel.php
+++ model/GameModel.php
@@ -279,6 +279,7 @@ class GameModel
             ];
         }
     }
+}
     private function endGame($idMatch)
     {
         $sqlEndGame = "UPDATE game SET estado = 'finalizada', fecha_fin = NOW() WHERE id = ?";
@@ -343,80 +344,3 @@ class GameModel
         }
         return null;
     }
-
-    public function getRanking(){
-        $sql = "SELECT DISTINCT u.id, u.username, ug.puntaje
-               FROM user_game ug JOIN user u ON ug.user_id = u.id
-               GROUP BY u.id 
-               ORDER BY ug.puntaje DESC";
-
-    $stmt = $this->database->prepare($sql);
-    $stmt->execute();
-        $result = $stmt->get_result();
-        $resultrows = $result->fetch_all(MYSQLI_ASSOC);
-
-        return $resultrows;
-    }
-
-    public function getPosition($idUser)
-    {
-        $sql = "SELECT *
-FROM (
-    SELECT
-        u.id,
-        g.puntaje,
-        RANK() OVER (ORDER BY g.puntaje DESC) AS posicion
-    FROM
-        user u
-    JOIN
-        user_game g ON u.id = g.user_id
-) AS ranking
-WHERE
-    id = ?;";
-
-
-        $stmt = $this->database->prepare($sql);
-        $stmt->bind_param("i", $idUser);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $resultFinal = $result->fetch_assoc();
-
-        return $resultFinal;
-    }
-
-    public function getDataProfile($idUser)
-        //foto perfil - agregar bdd
-        //Nombre de usuario
-        //Mejor partidas (historico)
-        //trampitas - agregar bdd
-    {
-        $sql = "SELECT DISTINCT u.id, u.username, MAX(ug.puntaje)
-               FROM user_game ug JOIN user u ON ug.user_id = u.id
-               WHERE u.id = ?";
-
-        $stmt = $this->database->prepare($sql);
-        $stmt->bind_param("i", $idUser);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_assoc();
-    }
-
-    public function getProfile($idUser){
-        //foto perfil
-        //Nombre de usuario
-        //Mejor partidad (historico)
-        //posicion Ranking
-        //qr -- Se crea a parte
-        //porcentaje de partidas (ganadas/perdidas) -- Ver si tiene sentido
-        //trampitas -- agregar bdd
-
-        $position = $this->getPosition($idUser);
-        $dataProfile = $this->getDataProfile($idUser);
-
-        return ['position'=>$position, 'dataProfile'=>$dataProfile];
-    }
-
-
-
-}
